68aa6415191fdb4114ad261a96e4a79916c3c796
kurivill <kurikkaville@hotmail.fi>
2025-02-26 13:16:55 +0200

    TempConvertor and tests done

---
+++ src/test/java/TemperatureConvertorTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TemperatureConvertorTest {
+
+    TemperatureConvertor converter = new TemperatureConvertor();
+
+
+    @Test
+    public void testFahrenheitToCelsius() {
+        assertEquals(0, converter.fahrenheitToCelsius(32), 0.001);
+        assertEquals(100, converter.fahrenheitToCelsius(212), 0.001);
+        assertEquals(-40, converter.fahrenheitToCelsius(-40), 0.001);
+    }
+
+    @Test
+    public void testCelsiusToFahrenheit() {
+        assertEquals(32, converter.celsiusToFahrenheit(0), 0.001);
+        assertEquals(212, converter.celsiusToFahrenheit(100), 0.001);
+        assertEquals(-40, converter.celsiusToFahrenheit(-40), 0.001);
+    }
+
+    @Test
+    public void testIsExtremeTemperature() {
+        assertTrue(converter.isExtremeTemperature(-50));
+        assertFalse(converter.isExtremeTemperature(20));
+        assertTrue(converter.isExtremeTemperature(60));
+        assertFalse(converter.isExtremeTemperature(-5));
+    }
+}
